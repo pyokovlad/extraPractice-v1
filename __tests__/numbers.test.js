671e829f2f585b6c21cee9bedd8363edbda6f846
Lucy Tsvetkova <pooogh9@gmail.com>
2023-10-28 14:15:12 +0300

    tasks

--- __tests__/numbers.test.js
+++ __tests__/numbers.test.js
@@ -39,6 +39,32 @@ import {
   areSameColorFields
 } from '../boolean';
 
+import {
+  incrementIfPositive,
+  getIndexOfSmallerNumber,
+  findMiddleNumber,
+  findUniqueNumber,
+  calculateF,
+  describeNumber,
+  convertToMeters,
+  describeAge,
+  describeNumber2
+} from '../branching';
+
+import {
+  sumOfSquares,
+  power,
+  factorialProduct,
+  fibonacciSequence,
+  sumOfPowers,
+  remainingLength,
+  findExponent,
+  findMinKAndSum,
+  calculateGCD,
+  findFibonacciIndex
+} from '../loops';
+
+
 describe('Group: numbers', () => {
   test('task1', () => {
     expect(calculateDistance(0, 5)).toBe(5);
@@ -398,5 +424,193 @@ describe('Group: boolean', () => {
     expect(areSameColorFields(1, 2, 2, 1)).toBe(true);
 
     expect(areSameColorFields(4, 7, 6, 5)).toBe(true);
+  });
+});
+
+describe('Group: branching', () => {
+  test('task1', () => {
+    expect(incrementIfPositive(5)).toBe(6);
+
+    expect(incrementIfPositive(0)).toBe(0);
+
+    expect(incrementIfPositive(-3)).toBe(-3);
+
+    expect(incrementIfPositive(2.5)).toBe(3.5);
+
+    expect(incrementIfPositive(-1.8)).toBe(-1.8);
+  });
+
+  test('task2', () => {
+    expect(getIndexOfSmallerNumber(3, 7)).toBe(1);
+
+    expect(getIndexOfSmallerNumber(-5, 2)).toBe(1);
+
+    expect(getIndexOfSmallerNumber(4, 4)).toBe(1);
+
+    expect(getIndexOfSmallerNumber(0, -1)).toBe(1);
+
+    expect(getIndexOfSmallerNumber(8.5, 8.8)).toBe(1);
+  }); 
+  
+  test('task3', () => {
+    expect(findMiddleNumber(2, 8, 4)).toBe(4);
+
+    expect(findMiddleNumber(-3, 0, 5)).toBe(0);
+
+    expect(findMiddleNumber(1, 1, 1)).toBe(1);
+
+    expect(findMiddleNumber(-2, -5, -1)).toBe(-2);
+
+    expect(findMiddleNumber(7.5, 6.2, 8.8)).toBe(7.5);
+  });
+
+  test('task4', () => {
+    expect(findUniqueNumber(5, 5, 5, 2)).toBe(4);
+
+    expect(findUniqueNumber(-3, -1, -1, -1)).toBe(1);
+
+    expect(findUniqueNumber(7, 9, 7, 7)).toBe(2);
+
+    expect(findUniqueNumber(2, 2, 2, 3)).toBe(4);
+  });
+
+  test('task5', () => {
+    expect(calculateF(-3)).toBe(0);
+
+    expect(calculateF(0.5)).toBe(1);
+
+    expect(calculateF(1.5)).toBe(-1);
+
+    expect(calculateF(2.7)).toBe(1);
+  });
+
+  test('task6', () => {
+    expect(describeNumber(24)).toBe('четное двузначное число');
+
+    expect(describeNumber(37)).toBe('нечетное двузначное число');
+
+    expect(describeNumber(108)).toBe('четное трехзначное число');
+
+    expect(describeNumber(215)).toBe('нечетное трехзначное число');
+  });
+
+  test('task7', () => {
+    expect(getDayName(1)).toBe('Monday');
+
+    expect(getDayName(2)).toBe('Tuesday');
+
+    expect(getDayName(3)).toBe('Wednesday');
+
+    expect(getDayName(4)).toBe('Thursday');
+
+    expect(getDayName(5)).toBe('Friday');
+
+    expect(getDayName(6)).toBe('Saturday');
+
+    expect(getDayName(7)).toBe('Sunday');
+
+    expect(getDayName(3.5)).toBe('Invalid day number.');
+
+    expect(getDayName(0)).toBe('Invalid day number.');
+
+    expect(getDayName(8)).toBe('Invalid day number.');
+  });
+
+  test('task8', () => {
+    expect(convertToMeters(1, 5)).toBe(0.5);
+
+    expect(convertToMeters(2, 2.5)).toBe(2500);
+
+    expect(convertToMeters(3, 8)).toBe(8);
+
+    expect(convertToMeters(4, 1200)).toBe(1.2);
+
+    expect(convertToMeters(5, 30)).toBe(0.3);
+  });
+
+  test('task9', () => {
+    expect(describeAge(20)).toBe('двадцать лет');
+    expect(describeAge(32)).toBe('тридцать два года');
+    expect(describeAge(41)).toBe('сорок один год');
+    expect(describeAge(56)).toBe('пятьдесят шесть лет');
+    expect(describeAge(69)).toBe('шестьдесят девять лет');
+  });
+
+  test('task10', () => {
+    expect(describeNumber2(100)).toBe('сто');
+    expect(describeNumber2(325)).toBe('триста двадцать пять');
+    expect(describeNumber2(511)).toBe('пятьсот одиннадцать');
+    expect(describeNumber2(999)).toBe('девятьсот девяносто девять');
+  });
+});
+
+describe('Group: loops', () => {
+  test('task1', () => {
+    expect(sumOfSquares(1, 3)).toBe(14);
+    expect(sumOfSquares(2, 5)).toBe(54);
+    expect(sumOfSquares(1, 5)).toBe(55);
+    expect(sumOfSquares(0, 4)).toBe(30);
+  });
+
+  test('task2', () => {
+    expect(power(2, 3)).toBe(8);
+    expect(power(3, 4)).toBe(81);
+    expect(power(5, 2)).toBe(25);
+    expect(power(10, 1)).toBe(10);
+  });
+
+  test('task3', () => {
+    expect(factorialProduct(1)).toBe(1);
+    expect(factorialProduct(2)).toBe(2);
+    expect(factorialProduct(4)).toBe(24);
+    expect(factorialProduct(5)).toBe(120);
+  });
+
+  test('task4', () => {
+    expect(fibonacciSequence(1)).toBe(1);
+    expect(fibonacciSequence(2)).toBe(1);
+    expect(fibonacciSequence(10)).toBe(89);
+    expect(fibonacciSequence(6)).toBe(13);
+  });
+
+  test('task5', () => {
+    expect(sumOfPowers(1)).toBe(1);
+    expect(sumOfPowers(2)).toBe(5);
+    expect(sumOfPowers(3)).toBe(32);
+    expect(sumOfPowers(4)).toBe(288);
+  });
+
+  test('task6', () => {
+    expect(remainingLength(10, 3)).toBe(1);
+    expect(remainingLength(15, 4)).toBe(3);
+    expect(remainingLength(20, 7)).toBe(6);
+    expect(remainingLength(30, 5)).toBe(0);
+  });
+
+  test('task7', () => {
+    expect(findExponent(2)).toBe(1);
+    expect(findExponent(8)).toBe(3);
+    expect(findExponent(16)).toBe(4);
+    expect(findExponent(64)).toBe(6);
+  });
+
+  test('task8', () => {
+    expect(findMinKAndSum(3)).toBe(2);
+    expect(findMinKAndSum(10)).toBe(4);
+    expect(findMinKAndSum(20)).toBe(6);
+    expect(findMinKAndSum(30)).toBe(7);
+  });
+
+  test('task9', () => {
+    expect(calculateGCD(24, 18)).toBe(6);
+    expect(calculateGCD(48, 60)).toBe(12);
+    expect(calculateGCD(35, 21)).toBe(7);
+  });
+
+  test('task10', () => {
+    expect(findFibonacciIndex(1)).toBe(1);
+    expect(findFibonacciIndex(2)).toBe(3);
+    expect(findFibonacciIndex(13)).toBe(7);
+    expect(findFibonacciIndex(89)).toBe(11);
   });
 });
